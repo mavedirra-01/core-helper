b1d5d2c7377badd56f6c0cf3ccd0e2996bdfd5a1
mavedirra-01 <mavedirra@gmail.com>
2023-02-09 18:07:49 -0400

    unsupported OS and other formatting changes

--- nmb.py
+++ nmb.py
@@ -45,9 +45,9 @@ msf_check = 0
 
 argc = len(sys.argv)
 if argc == 1:
-    print("\n",yellow,bold,"Usage",rc,": nmb.py /path/to/nessus.csv")
+    print(yellow,bold,"Usage",rc,": nmb.py /path/to/nessus.csv","\n")
     print(bold,"Additional options:", rc)
-    print("",blue, bold,"--msf\t",rc," run script with metasploit checks enabled", bold, "Note:", rc, "this will be much slower")
+    print(blue,bold,"--msf\t",rc," run script with metasploit checks enabled (this will be much slower)")
     sys.exit()
 file = sys.argv[1]
 if os.path.isfile(file):
@@ -56,9 +56,9 @@ else:
     print(red, "File does not exist:", file)
 if not file.endswith(".csv"):
     print(red, "Error: The file must be of type .csv")
-    print("\n",yellow,bold,"Usage",rc,": nmb.py /path/to/nessus.csv")
+    print(yellow,bold,"Usage",rc,": nmb.py /path/to/nessus.csv","\n")
     print(bold,"Additional options:", rc)
-    print("",blue, bold,"--msf\t",rc," run script with metasploit checks enabled", bold, "Note:", rc, "this will be much slower")
+    print(blue,bold,"--msf\t",rc," run script with metasploit checks enabled (this will be much slower)")
     exit()
 if argc == 2:
     print(green, "File exists and is in correct format:", file)
@@ -184,6 +184,57 @@ def nmap_verify_version(plugin_id, output_file):
         shutil.move(tmp_file, dst_file)
 
 
+###################################################################################################################################
+
+def nmap_verify_os_version(plugin_id, output_file):
+# Open the Nessus .csv file and read it
+    ips = []
+    with open(file, 'r', encoding="utf8") as f:
+        reader = csv.reader(f)
+
+        # Iterate through each row
+        for row in reader:
+            # Check if the plugin ID in column 1 matches the specified value
+            #matching_plugin_id = "153953" ### weak key exchange SSH
+            if row[0] in plugin_id:
+                name = row[7]
+                # Extract the IP from column 4 and the port from column 6
+                ip = row[4]
+                # Append the IP and port to the respective lists
+                ips.append(ip)
+                # Delete all ips except the first
+                ips = [ips[0]]
+
+    # Iterate through the ips and ports
+    for i in range(len(ips)):
+        ip = ips[i]
+        # Pass the ip and port to nmap
+        nmap_output = subprocess.run([f'sudo nmap -Pn -sC -O -sV {ip} &'], capture_output=True, shell=True)
+        with open(output_file, "w") as f:
+                f.write(nmap_output.stdout.decode())
+        with open(output_file, "r") as f:
+            content = f.read()
+        if "No exact OS matches for host" in content:
+            print(red, "Error: No exact OS matches for host -", name)
+        else:
+            print(green, "Finding:", name, "Verified")
+    if os.path.isfile(output_file):
+        # Removal of empty lines 
+        tmp_file="tmp.txt"
+        with open(output_file, "r") as original, open(tmp_file, "w") as temp:
+        # iterate over the lines in the original file
+            for line in original:
+            # check if the line is not empty
+                if line.strip():
+                # write the non-empty line to the temporary file
+                    temp.write(line)
+    # remove the original file
+        os.remove(output_file)
+    # move the temporary file to the original file's location
+        dst_file = os.path.join(make_evidence, os.path.basename(output_file))
+        shutil.move(tmp_file, dst_file)
+
+
 
 ######################################################################################################################################
 def nmap_verify_script(plugin_id, script, output_file):
@@ -224,7 +275,6 @@ def nmap_verify_script(plugin_id, script, output_file):
             print(green, "Finding:", name, "Verified") #### add something about successful verification of (name)
 
     # Create file incase plugin doesn't exist in file 
-    #open(output_file, "a").close()
     # Removal of empty lines
     if os.path.isfile(output_file):
         tmp_file="tmp.txt"
@@ -309,7 +359,8 @@ nmap_verify_version(["58987","166901", "161971", "165545"], "php_version.txt") #
 nmap_verify_version(["150280", "153583", "156255", "158900", "161454", "161948", "170113", "153585", "153586"], "apache_version.txt") # Apache version
 nmap_verify_version(["152782", "160477", "162420", "148125", "148402", "158974", "144047", "157228", "162721"], "openssl_version.txt") # Openssl version
 nmap_verify_version(["72692", "95438", "121119", "133845", "66428", "72691", "74247", "74246", "77475", "83764", "88936", "88936", "94578", "96003", "99367", "100681", "103329", "103329", "103698", "103782", "106975", "118035", "12116", "12117", "12118", "121120", "121121", "136770", "138851", "147163", "148405", "151502"], "tomcat_version.txt")
-
+nmap_verify_os_version("108797", "windows_os_version.txt") # Unsupported windows OS
+nmap_verify_os_version("33850", "unix_os_version.txt") # Unsupported unix OS
 
 #########################################################################################
 if msf_check == 1:
