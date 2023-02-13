e6bde8a9f092f494c4a2d8eedba1a380dc7e7e21
mavedirra-01 <mavedirra@gmail.com>
2023-02-13 13:25:33 -0400

    merge into main nmb

--- nmb.py
+++ nmb.py
@@ -39,6 +39,7 @@ rc = '\033[0m'
     - if path to file is valid but file is not .csv == error 
     - if user inputs incorrect arg == error
 """
+# Globals
 found = False
 run_msf = 0
 msf_check = 0
@@ -123,6 +124,9 @@ def custom_verify(plugin_id, script_args, output_file):
             if i == len(ips) - 1:
                 print(red, "Error: All IP addresses are down -", name)
                 break
+        elif not content:
+            print(yellow, "Unable to verify content please review manually -", name)
+            break
         else:
             found = True
             print(green, "Finding:", name, bold,"Verified",rc)
@@ -144,8 +148,8 @@ def custom_verify(plugin_id, script_args, output_file):
         shutil.move(tmp_file, dst_file)
 
 #############################################################
-def nmap_verify_version(plugin_id, output_file):
-# Open the Nessus .csv file and read it
+def nmap_verify(plugin_id, args, output_file):
+    # Open the Nessus .csv file and read it
     ips = []
     ports = []
     global found
@@ -172,14 +176,14 @@ def nmap_verify_version(plugin_id, output_file):
         ip = ips[i]
         port = ports[i]
         # Pass the ip and port to nmap
-        nmap_output = subprocess.run([f'nmap -Pn -sC -sV -p {port} {ip} &'], capture_output=True, shell=True)
+        nmap_output = subprocess.run([f'nmap -Pn -T4 {args} {port} {ip} &'], capture_output=True, shell=True)
         with open(output_file, "w") as f:
                 f.write(nmap_output.stdout.decode())
         with open(output_file, "r") as f:
             content = f.read()
         if "filtered" in content:
             if i == len(ips) - 1:
-                print(red, "Error: All IP addresses are down -", name)
+                print(red, "Error: All IP addresses might be down, please review results manually -", name)
                 break
         else:
             print(green, "Finding:", name, bold,"Verified",rc)
@@ -204,65 +208,8 @@ def nmap_verify_version(plugin_id, output_file):
 
 ###################################################################################################################################
 
-def nmap_verify_os_version(plugin_id, output_file):
-# Open the Nessus .csv file and read it
-    ips = []
-    global found
-    with open(file, 'r', encoding="utf8") as f:
-        reader = csv.reader(f)
-
-        # Iterate through each row
-        for row in reader:
-            # Check if the plugin ID in column 1 matches the specified value
-            #matching_plugin_id = "153953" ### weak key exchange SSH
-            if row[0] in plugin_id:
-                name = row[7]
-                # Extract the IP from column 4 and the port from column 6
-                ip = row[4]
-                # Append the IP and port to the respective lists
-                ips.append(ip)
-                # Delete all ips except the first
-                # ips = [ips[0]]
-
-    # Iterate through the ips and ports
-    found = False
-    for i in range(len(ips)):
-        ip = ips[i]
-        # Pass the ip and port to nmap
-        nmap_output = subprocess.run([f'sudo nmap -Pn -sC -O -sV {ip} &'], capture_output=True, shell=True)
-        with open(output_file, "w") as f:
-                f.write(nmap_output.stdout.decode())
-        with open(output_file, "r") as f:
-            content = f.read()
-        if "No exact OS matches for host" in content:
-            if i == len(ips) - 1:
-                print(red, "Error: All IP addresses are down -", name)
-                break
-        else:
-            print(green, "Finding:", name, bold,"Verified",rc)
-            found = True
-            break
-    if os.path.isfile(output_file):
-        # Removal of empty lines 
-        tmp_file="tmp.txt"
-        with open(output_file, "r") as original, open(tmp_file, "w") as temp:
-        # iterate over the lines in the original file
-            for line in original:
-            # check if the line is not empty
-                if line.strip():
-                # write the non-empty line to the temporary file
-                    temp.write(line)
-    # remove the original file
-        os.remove(output_file)
-    # move the temporary file to the original file's location
-        dst_file = os.path.join(make_evidence, os.path.basename(output_file))
-        shutil.move(tmp_file, dst_file)
-
-
-
-######################################################################################################################################
-def nmap_verify_script(plugin_id, script, output_file):
-# Open the Nessus .csv file and read it
+def nmap_verify_sudo(plugin_id, args, output_file):
+    # Open the Nessus .csv file and read it
     ips = []
     ports = []
     global found
@@ -283,26 +230,27 @@ def nmap_verify_script(plugin_id, script, output_file):
                 ports.append(port)
                 # Delete all ips except the first
                 # ips = [ips[0]]
+
     # Iterate through the ips and ports
     for i in range(len(ips)):
         ip = ips[i]
         port = ports[i]
         # Pass the ip and port to nmap
-        nmap_output = subprocess.run([f'nmap -Pn --script {script} -p {port} {ip} &'], capture_output=True, shell=True)
+        nmap_output = subprocess.run([f'sudo nmap -T4 -Pn {args} {ip} &'], capture_output=True, shell=True)
         with open(output_file, "w") as f:
                 f.write(nmap_output.stdout.decode())
         with open(output_file, "r") as f:
             content = f.read()
-        if "filtered" in content:
+        if "No exact OS matches for host" or "Too many fingerprints match this host" in content:
             if i == len(ips) - 1:
-                print(red, "Error: All IP addresses are down -", name)
+                print(red, "Error: All IP addresses might be down, please review results manually -", name)
                 break
         else:
-            print(green, "Finding:", name, bold,"Verified",rc) 
+            print(green, "Finding:", name, bold,"Verified",rc)
             found = True
-            break 
-    # Removal of empty lines
+            break
     if os.path.isfile(output_file):
+        # Removal of empty lines 
         tmp_file="tmp.txt"
         with open(output_file, "r") as original, open(tmp_file, "w") as temp:
         # iterate over the lines in the original file
@@ -350,7 +298,7 @@ def msfconsole_verify(plugin_id, module, output_file):
 
 # Run Metasploit command
     if run_msf == 1:
-        cmd = f"msfconsole -x 'use {module}; set RHOST {ip}; set RPORT {port}; run; exit -y' > {output_file}"
+        cmd = f"sudo msfconsole -x 'use {module}; set RHOST {ip}; set RPORT {port}; run; exit -y' > {output_file}"
         subprocess.run(cmd, shell=True)
         dst_file = os.path.join(make_evidence, os.path.basename(output_file))
         shutil.move(output_file, dst_file)
@@ -401,17 +349,17 @@ if query == 1:
 custom_verify("41028", "snmp-check -v 2c -c public -w", "snmp_check.txt") # snmp public write test and info gather
 custom_verify("57608", "crackmapexec smb --gen-relay-list smb_targets.txt", "smb_targets.txt") # SMB signing not required
 custom_verify("97861", "sudo ntpd -c monlist", "ntp_mode6.txt") # NTP Mode 6 scanner
-nmap_verify_script("104743", "ssl-enum-ciphers", "tls_version.txt") # tls version
-nmap_verify_script(['51192', '20007', '57582', '15901'], "ssl-cert", "ssl_cert.txt") # ssl cant be trusted, SSL v2/3, self-signed ssl, ssl cert expiry
-nmap_verify_script(["70658", "153953", "71049"], "ssh2-enum-algos", "ssh_ciphers.txt") # SSH cbc ciphers, SSH weak-keyx, SSH MAC algos
-nmap_verify_version("138475", "esxi_version.txt") # esxi version
-nmap_verify_version("168746", "vcenter_version.txt") # VMware vcenter version
-nmap_verify_version(["58987","166901", "161971", "165545"], "php_version.txt") # PHP unsupported version detection
-nmap_verify_version(["150280", "153583", "156255", "158900", "161454", "161948", "170113", "153585", "153586"], "apache_version.txt") # Apache version
-nmap_verify_version(["152782", "160477", "162420", "148125", "148402", "158974", "144047", "157228", "162721"], "openssl_version.txt") # Openssl version
-nmap_verify_version(["72692", "95438", "121119", "133845", "66428", "72691", "74247", "74246", "77475", "83764", "88936", "88936", "94578", "96003", "99367", "100681", "103329", "103329", "103698", "103782", "106975", "118035", "12116", "12117", "12118", "121120", "121121", "136770", "138851", "147163", "148405", "151502"], "tomcat_version.txt")
-nmap_verify_os_version("108797", "windows_os_version.txt") # Unsupported windows OS
-nmap_verify_os_version("33850", "unix_os_version.txt") # Unsupported unix OS
+nmap_verify("104743", "ssl-enum-ciphers", "tls_version.txt") # tls version
+nmap_verify(['51192', '20007', '57582', '15901'], "ssl-cert", "ssl_cert.txt") # ssl cant be trusted, SSL v2/3, self-signed ssl, ssl cert expiry
+nmap_verify(["70658", "153953", "71049"], "ssh2-enum-algos", "ssh_ciphers.txt") # SSH cbc ciphers, SSH weak-keyx, SSH MAC algos
+nmap_verify("138475", "esxi_version.txt") # esxi version
+nmap_verify("168746", "vcenter_version.txt") # VMware vcenter version
+nmap_verify(["58987","166901", "161971", "165545"], "php_version.txt") # PHP unsupported version detection
+nmap_verify(["150280", "153583", "156255", "158900", "161454", "161948", "170113", "153585", "153586"], "apache_version.txt") # Apache version
+nmap_verify(["152782", "160477", "162420", "148125", "148402", "158974", "144047", "157228", "162721"], "openssl_version.txt") # Openssl version
+nmap_verify(["72692", "95438", "121119", "133845", "66428", "72691", "74247", "74246", "77475", "83764", "88936", "88936", "94578", "96003", "99367", "100681", "103329", "103329", "103698", "103782", "106975", "118035", "12116", "12117", "12118", "121120", "121121", "136770", "138851", "147163", "148405", "151502"], "tomcat_version.txt")
+nmap_verify_sudo("108797", "windows_os_version.txt") # Unsupported windows OS
+nmap_verify_sudo("33850", "unix_os_version.txt") # Unsupported unix OS
 
 #########################################################################################
 if msf_check == 1:
