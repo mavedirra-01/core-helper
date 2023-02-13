ea8ab505e34d5772685d3da29799ed0b21c920da
mavedirra-01 <mavedirra@gmail.com>
2023-02-13 18:31:31 -0400

    quick fix

--- nmb.py
+++ nmb.py
@@ -218,7 +218,7 @@ def nmap_verify(plugin_id, args, output_file):
                 f.write(nmap_output.stdout.decode())
         with open(output_file, "r") as f:
             content = f.read()
-        if "Host seems down" in content:
+        if "Host seems down" or "closed" or "filtered" in content:
             if i == len(ips) - 1:
                 print(red, "Error: All IP addresses might be down, please review results manually -", name)
                 break
@@ -389,8 +389,8 @@ nmap_verify(["150280", "153583", "156255", "158900", "161454", "161948", "170113
 nmap_verify(["152782", "160477", "162420", "148125", "148402", "158974", "144047", "157228", "162721"], "-sC -sV -p", "openssl_version.txt") # Openssl version
 nmap_verify(["72692", "95438", "121119", "133845", "66428", "72691", "74247", "74246", "77475", "83764", "88936", "88936", "94578", "96003", "99367", "100681", "103329", "103329", "103698", "103782", "106975", "118035", "12116", "12117", "12118", "121120", "121121", "136770", "138851", "147163", "148405", "151502"], "-sC -sV -p", "tomcat_version.txt")
 #custom_verify("97861", "sudo ntpd -c monlist", "ntp_mode6.txt") # NTP Mode 6 scanner
-nmap_verify_sudo("33850", "-sC -sV -O", "unix_os_version.txt") # Unsupported unix OS
-nmap_verify_sudo("108797", "-sC -sV -O", "windows_os_version.txt") # Unsupported windows OS
+# nmap_verify_sudo("33850", "-sC -sV -O", "unix_os_version.txt") # Unsupported unix OS
+# nmap_verify_sudo("108797", "-sC -sV -O", "windows_os_version.txt") # Unsupported windows OS
 
 #########################################################################################
 if msf_check == 1:
