2e3435eb09cdfe800fbe9fb3ddf6d41c1bee95bb
mavedirra-01 <mavedirra@gmail.com>
2023-02-17 15:33:47 -0400

    d

--- inprog.py
+++ inprog.py
@@ -554,23 +554,10 @@ class Nessus:
 			ui_version = version_info.get("nessus_ui_version")
 			
 			if re.match(r"^8(\.|$)", ui_version):
-				# responce = requests.get(self.url + f"/scans/{scan_id}/export/formats", headers=self.token_auth, verify=False)
-				# templates = json.loads(responce.text)
-				template_id = "Vulnerabilites By Plugin"
+				template_id = "214"
 				
 			else:
 				template_id = "214"
-				# response = requests.get(self.url + f"/reports/custom/templates", headers=self.token_auth, verify=False)
-				# templates = json.loads(response.text)
-				#print(templates)
-				
-			# get html template id
-				# for template in templates:
-				# 	print(template)
-				# 	if template["name"] == "Detailed Vulnerabilites By Plugin":
-				# 		template_id = template["id"]
-				# 		print(template_id)
-				# 		break
 											
 			# format handlers
 			formats = {
