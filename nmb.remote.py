e3be52ee1afce68baa2165c2df9d03bdee6c185c
mavedirra-01 <mavedirra@gmail.com>
2023-02-22 15:08:55 -0400

    w

--- nmb.remote.py
+++ nmb.remote.py
@@ -763,10 +763,10 @@ class Nessus:
             response = requests.get(self.url + f"/reports/custom/templates", headers=self.token_auth, verify=False)
             templates = json.loads(response.text)
             for template in templates:
-                if template["name"] == "Detailed Vulnerabilites By Plugin":
-                    template_id = template["id"]
-                    print(template_id)
-                    break	
+                # if template["name"] == "Detailed Vulnerabilites By Plugin":
+                template_id = template["id"]
+                print(template_id, template["name"])
+                break	
 
 
             # format handlers
