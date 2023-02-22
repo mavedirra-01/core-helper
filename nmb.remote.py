32df52aff38195dd45716a8655d01652b0be059a
mavedirra-01 <mavedirra@gmail.com>
2023-02-22 15:00:03 -0400

    d

--- nmb.remote.py
+++ nmb.remote.py
@@ -762,10 +762,11 @@ class Nessus:
 # ############ Removed the below code as the url is different between nessus 8 and nessus 10 but the template ID is the same
             response = requests.get(self.url + f"/reports/custom/templates", headers=self.token_auth, verify=False)
             templates = json.loads(response.text)
+            print(templates)
             for template in templates:
                 if template["name"] == "Complete List of Vulnerabilities by Host":
                     template_id = template["id"]
-                    print(template_id)
+                    # print(template_id)
                     break	
 
 
